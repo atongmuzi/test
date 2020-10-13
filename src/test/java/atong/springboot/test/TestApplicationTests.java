cb5627064597e190c435ea686552635dcdea2c97
atongmuzi <jazx505@163.com>
2020-10-13 11:41:37 +0800

    basehhaah--wowoow

--- src/test/java/atong/springboot/test/TestApplicationTests.java
+++ src/test/java/atong/springboot/test/TestApplicationTests.java
@@ -41,6 +41,7 @@ public class TestApplicationTests extends AbstractTestNGSpringContextTests {
         System.out.println("lalalahahah--atong");
         System.out.println("lalalahahah--atong");
         System.out.println("lalalahahah--atong");
+        System.out.println("lalalahahah--atong");
 
 
 
