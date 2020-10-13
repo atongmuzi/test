1e8b0c8bf7e0f55c620f787151b1e0e48b132f6b
atongmuzi <jazx505@163.com>
2020-10-13 11:58:40 +0800

    basehhaah--wowoow

--- src/test/java/atong/springboot/test/TestApplicationTests.java
+++ src/test/java/atong/springboot/test/TestApplicationTests.java
@@ -34,17 +34,7 @@ public class TestApplicationTests extends AbstractTestNGSpringContextTests {
 //        String url2 = this.env.getProperty("spring.my-list.url[1]");
 //        System.out.println(isPort + " " + url1 + " "+url2);
         System.out.println("ahhahha");
-        System.out.println("lalall");
-        System.out.println("lalalahahah");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
-        System.out.println("lalalahahah--atong");
+
 
 
 
